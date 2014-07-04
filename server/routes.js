b8322ab9db04ddedbd621bbec2dbbd89d637bbd1
Richard Neil Ilagan <richardilagan@gmail.com>
2014-07-04 13:05:38 +0800

    Slides. Milestone.

--- server/routes.js
+++ server/routes.js
@@ -9,14 +9,14 @@ module.exports = function (app) {
   // :: API
 
   // # uncomment this to allow CORS for all domains
-  app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    //res.header('Access-Control-Max-Age', '3600');
+  // app.use(function (req, res, next) {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  //   //res.header('Access-Control-Max-Age', '3600');
 
-    next();
-  });
+  //   next();
+  // });
 
   app.route('/api/foo')
     .all(api.foo)
